d0c08b291b2d2010d9afe2262bd68302e4929d21
EricDkim <dkim3@ggc.edu>
2016-08-18 13:35:13 -0400

    Added my folder

---
+++ EricDkim/my_name.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#adding a bash script that prints my name
+
+echo "Welcome, what is your name good sir?"
+read name
+echo "Hi there, $name"
+
+echo 'I have stored your name into the $name'
