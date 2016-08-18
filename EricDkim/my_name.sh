7f0cb165d15d75162228999486dce2544ef909f6
EricDkim <dkim3@ggc.edu>
2016-08-18 13:38:12 -0400

    Corrected the bash script

--- EricDkim/my_name.sh
+++ EricDkim/my_name.sh
@@ -2,7 +2,4 @@
 #adding a bash script that prints my name
 
 echo "Welcome, what is your name good sir?"
-read name
-echo "Hi there, $name"
-
-echo 'I have stored your name into the $name'
+echo "My name is Eric D. Kim."
