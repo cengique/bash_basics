a67e45dbc0f8cf4ed6ad151004d83cb8bc89e62e
Wes-McDorman <wmcdorma@ggc.edu>
2016-08-24 11:30:39 -0400

    Did I fix it?

---
+++ Wes/01-read_input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello Wes!"
+
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
