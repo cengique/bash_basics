4fa23c96f17fe9a99e6a237c2da6de9d0b3ceb16
akonee1 <akone1@ggc.edu>
2016-08-29 18:03:45 -0400

    agmt2

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,33 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
+#import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * Class: filecreate
+ * @author Aboubakar Winston Kone
+ * @version 1.0 
+ * Course: ITEC 3870 Section 1 Dr.CengizGunay
+ */
+
+public class filecreate {
+	public static void main(String[] args) throws IOException
+	{ 
+
+		try {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter a file name: ");
+			String fileName = input.nextLine();
+			File file = new File(fileName + ".txt");
+			if (file.createNewFile())
+				System.out.println("File created");
+			else
+				System.out.println("Error");
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+	}
+}
