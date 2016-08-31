809d0ec1c55ff3bcc5b593ae80ff103fd4129f70
Jawan Higgins <Jhiggin3@ggc.edu>
2016-08-31 12:40:18 -0400

    Second Commit

--- JawanHiggins.sh
+++ JawanHiggins.sh
@@ -5,6 +5,8 @@ echo "Hello! What is your name"
 read name
 echo "Welcome, $name"
 
+
+
 #Example Script
 echo "What is the meaning of life?"
 read meaning
@@ -16,6 +18,8 @@ else
    echo "Awww... You don't know the meaning of life"
 fi
 
+
+
 # exercise: write a script that prints whether it is morning or not
 
 echo "Is it AM or PM?"
@@ -24,7 +28,37 @@ read timeStatus
 if [[ "${timeStatus}" == "AM" || "${timeStatus}" == "am" ]]; then
     echo "It is morning"
 elif [[ "${timeStatus}" == "PM" || "${timeStatus}" == "pm" ]]; then 
-    echo "It is the evening."
+    echo "It is not the morning, it is the evening."
 else 
     echo "Invalid entry."
 fi 
+
+
+
+#Example Script 
+echo "Enter your first number"
+read first
+echo "Enter your second number"
+read second
+echo "Enter your third number"
+read third
+
+sum=$(( first+second+third ))
+
+echo "The sum is $sum"
+
+
+
+# exercise: ask the user for the width and height and present total
+# number of pixels
+
+echo "Enter width"
+read first
+echo "Enter height"
+read second
+echo "Enter total pixels"
+read pixels
+
+area=$(( first * second ))
+totalPix=$pixels
+echo "The Area is $area and the totals pixels are $totalPix"
