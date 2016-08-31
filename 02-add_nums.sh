f5d2ab3f9525deffc5928b2455c348091832d8a8
Xavier Lazo <xlazo@ggc.edu>
2016-08-31 18:48:48 -0400

    trying to merge

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Please enter dimesions for the width and height to get a total count of pixels"
+echo "Enter width"
+read width 
+echo "Enter height"
+read height
+
+totalPixels=$((width + height))
+
+echo "The total number of pixels is $totalPixels"
