ab348d07bef058f5aa889977b39ddf25fbcaed75
Jawan Higgins <Jhiggin3@ggc.edu>
2016-08-31 14:15:55 -0400

    Third Commit

--- JawanHiggins.sh
+++ JawanHiggins.sh
@@ -62,3 +62,42 @@ read pixels
 area=$(( first * second ))
 totalPix=$pixels
 echo "The Area is $area and the totals pixels are $totalPix"
+
+
+
+
+#Example Script
+
+echo "What did you get in the first ITEC 3860 test?"
+echo "Please give a numeric answer"
+read grade
+
+if [ $grade -ge 90 ]; then
+   echo "You got an A. Nice."
+elif [ $grade -ge 80 ]; then
+   echo "You got a B. That's good."
+elif [ $grade -ge 70 ]; then
+   echo "You got a C. Not bad."
+else
+   echo "Time to work on some extra credit assignments"
+fi
+
+
+
+# exercise: write a script (that utilizes weather-util) 
+# that prints "it's cold" if the temperature is < 40
+# it's chilly if < 60, it's okay if < 70 and, it's hot for 
+# everything else
+
+echo "What is the temperature outside?"
+read temp
+
+if [ $temp -lt 40 ]; then
+    echo "Its cold"
+elif [ $temp -lt 60 ]; then
+    echo "Its chilly"
+elif [ $temp -lt 70 ]; then
+    echo "Its okay"
+else
+    echo "Its hot"
+fi
