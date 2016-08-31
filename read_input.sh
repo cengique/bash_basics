0e883aaa268658703b4f32b29a6b86aa1f20547e
akonee1 <akone1@ggc.edu>
2016-08-31 17:57:56 -0400

    edited exercise

---
+++ read_input.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name
+echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * Class: filecreate
+ * @author Aboubakar Winston Kone
+ * @version 1.0 
+ * Course: ITEC 3870 Section 1 Dr.CengizGunay
+ */
+
+public class filecreate {
+	public static void main(String[] args) throws IOException
+	{ 
+
+		try {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter a file name: ");
+			String fileName = input.nextLine();
+			File file = new File(fileName + ".txt");
+			if (file.createNewFile())
+				System.out.println("File created");
+			else
+				System.out.println("Error");
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+	}
+}
