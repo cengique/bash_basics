7cbd8cdccd1a5a368138430b4a9bdf5f4fd3f247
ltoornman <ltoornman@gmail.com>
2016-09-01 15:24:34 -0400

    Assignment2 Commit Testing

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -14,9 +14,9 @@ echo "The sum is $sum"
 # exercise: ask the user for the width and height and present total
 # number of pixels
 echo "Enter width in Pixels"
-read firstpixel
+read firstpixels
 echo "Enter length in Pixels"
-read secondpixel
+read secondpixels
 
 sum=$((firstpixel+secondpixel))
 echo "The total is $sum pixels"
