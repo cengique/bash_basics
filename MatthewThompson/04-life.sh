79ad0cc10330335cf8a844697677699538f0de2e
FatThompson <matthewbiggiethompson@gmail.com>
2016-09-01 16:32:23 -0400

    Causeing a colision

---
+++ MatthewThompson/04-life.sh
@@ -0,0 +1,22 @@
+
+# echo "What is the meaning of life?"
+# read meaning
+
+
+# if [ "$meaning" -eq 42 ]; then
+#    echo "Yes!, That is the meaning of life!"
+# else
+#    echo "Awww... You don't know the meaning of life"
+# fi
+
+#  here are some other arithemetic comparison operators
+# -eq -ne -gt -ge -lt -le
+
+# exercise: write a script that prints whether it is 
+# morning or not
+echo "ITSSS MORNGING GUYS"
+TIME=$(date +%H)
+if [ "$TIME" -lt 12 ]; then
+    echo "OHHHHHHHHHHHHHHHHYYYYYYYYYYYYAAAAAAAAAAAAAAAAAAAAAA"
+else echo "Just kidding, its the afternoon"
+fi
