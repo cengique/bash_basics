4895c933281fe98f0c81ddd74139c75833dfed70
Knagus <aseawell@ggc.edu>
2016-09-01 15:20:46 -0400

    Added a new script.
    Added a new script that asks the user for a file name that then creates
    a new .txt file with the name given.

---
+++ Austin/01-read_input-edit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+echo "Please enter a filename."
+read filename
+touch $filename.txt
+echo "A new .txt file named $filename has been created in the current directory." 
