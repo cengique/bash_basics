ee5fd3a99fdb25fea9c049408b53eaffaba5dfd9
Aston Tsou <atsou@ggc.edu>
2016-09-01 17:25:33 -0400

    change file a little

--- 04-life.sh
+++ 04-life.sh
@@ -14,3 +14,12 @@ fi
 
 # exercise: write a script that prints whether it is 
 # morning or not
+
+h=`date +%H`
+
+if [ $h -lt 12 ]; then
+  echo Good morning
+elif [ $h -lt 18 ]; then
+  echo Good afternoon
+else
+  echo Good evening
