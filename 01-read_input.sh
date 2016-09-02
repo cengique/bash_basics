8ac2d541b215f0cb4c6f6373325dc037c8a4e693
Palmer Simmons <psimmons1@ggc.edu>
2016-09-01 21:17:13 -0400

    Second branch commit

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -8,5 +8,11 @@ echo "Welcome, $name"
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
 
+echo "Please entor your filename"
+read filename
+
+touch $filename
+
+echo "There's something different here."
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
