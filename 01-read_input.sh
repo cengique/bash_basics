2f1eac7af3c1ecb5a592d04a259de2ea45b799eb
aabafogi <aabafogi@ggc.edu>
2016-09-06 22:12:52 -0400

    again modified read_input file

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -4,9 +4,11 @@
 echo "Hello! What is your name"
 read name
 echo "Welcome, $name"
-
+echo "what is your middle name"
+read middle name
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
+     'Your middle name was stored in $middle name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
