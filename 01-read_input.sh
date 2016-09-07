3ad0d27399d846606002e91e3c9fafbae6cc23f0
aabafogi <aabafogi@ggc.edu>
2016-09-06 21:58:19 -0400

    Modified read_input file

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -8,6 +8,6 @@ echo "what is your lastname"
 read lastname
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
-
+echo 'Your lastname stored in $lastname'
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
