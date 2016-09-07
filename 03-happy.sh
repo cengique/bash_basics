5b8e96c5be51da35660534ba3d486d6d10181ce9
aabafogi <aabafogi@ggc.edu>
2016-09-07 12:34:49 -0400

    happy.sh

--- 03-happy.sh
+++ 03-happy.sh
@@ -8,6 +8,13 @@ if [ "$answer" = "yes" ]; then
 else
    echo "Still Smile :)"
 fi
+echo "You are sad today?"
+read answer
+
+if ["$answer" = "no"]; then
+   echo "smile :)"
+else
+   echo "Still sad :)"
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
