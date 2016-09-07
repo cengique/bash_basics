9d0c709f9a8483443d1cc2544f414c68b616cf84
aabafogi <aabafogi@ggc.edu>
2016-09-07 12:55:14 -0400

    add change

--- 03-happy.sh
+++ 03-happy.sh
@@ -7,6 +7,14 @@ if [ "$answer" = "yes" ]; then
    echo "Smile :)"
 else
    echo "Still Smile :)"
+
+echo "You look angry?"
+ read answer
+
+if ["$answer" = "no"]; then
+   echo "smile :)"
+else
+ echo "still hangry :)"
 fi
 echo "You are sad today?"
 read answer
