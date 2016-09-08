16d979bcbe7792441bdb5c0fecdab6364e728bbd
aabafogi <aabafogi@ggc.edu>
2016-09-08 13:18:33 -0400

    conflict resloved

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -4,19 +4,10 @@
 echo "Hello! What is your name"
 read name
 echo "Welcome, $name"
-<<<<<<< HEAD
+
 echo "what is your lastname"
 read lastname
-# single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
-echo 'Your lastname stored in $lastname'
-=======
-echo "what is your middle name"
-read middle name
-# single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
-     'Your middle name was stored in $middle name'
 
->>>>>>> branchadam
+# single quotes prevent the expansion of the variable
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
