680abc430b3f6a58a1c53d8ae410ab87c3cb2d1c
EricDkim <dkim3@ggc.edu>
2016-09-10 23:27:04 -0400

    cleaned -6 script

--- 06-infinite.sh
+++ 06-infinite.sh
@@ -3,11 +3,11 @@
 # the if statement had, if-then-fi
 # while loop has, while-do-done
 # true and false are also Unix commands
-<<<<<<< HEAD
+
 #while [ false ]; do
 =======
 #while [ true ]; do
->>>>>>> practiceBranch
+
 #  echo "infinite number of beer on the wall"
 #done
 
@@ -15,7 +15,7 @@
 # the ip address of a given hostname (using nslookup) until
 # the user decides to stop
 
-<<<<<<< HEAD
+
 #nslookup unix.stackexchange.com | awk '/^Address: / { print $2 }'
 #nslookup google.com
 while [[ true ]]; do
