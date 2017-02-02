602fc7c5aab4883222f8422ba00e763c96bdaa9a
Josh_Gerth <jgerth@ggc.edu>
2017-02-01 21:20:53 -0500

    First edit of 02-add_nums.sh

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "We will now calculate the area of a rectangle"
+echo "Enter the width"
+read width
+echo "Enter the height"
+read height
+
+area=$(( width*height ))
+
+echo "The area is $area"
