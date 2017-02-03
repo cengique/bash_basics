5e09088bfe5371b120759f770eddd2990001dff1
Robert <rbryan21@gmail.com>
2017-02-03 11:06:46 -0500

    Added commands to 02-add_nums.sh

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,9 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Enter the width: "
+  read width
+echo "Enter the height: "
+  read height
+product=$(( width * height))
+echo "There are $product pixels on your screen"
