808f4fd0f75861d23b53be95d1226553427d6a4b
Jory <ralexander2@ggc.edu>
2017-02-03 15:55:46 -0500

    Added code to 02-add_nums.sh that prompts the user for the pixle width and height of a screen and gives back the number of pixles

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "enter the number of pixles in the width of your screen"
+read width
+echo "enter the number of pixles in the height of your screen"
+read height
+
+numPixles=$((width*height))
+
+echo "The total number of pixles in your screen is $numPixles"
