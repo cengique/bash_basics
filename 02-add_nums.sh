6cf98a2873b6f2b326b470d161a20b1e63daf8ec
Jory <ralexander2@ggc.edu>
2017-02-03 16:02:44 -0500

    added code to 02-add_nums that prints out the number of pixles in a screen as provided by the user

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -12,4 +12,13 @@ sum=$(( first+second+third ))
 echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
+
+echo "How many pixles in yo width?"
+read pixleW
+echo "How many pixles in yo height?"
+read pixleH
+
+pixleNum=$((pixleW*pixleH))
+
+echo "Number of pixles equals $pixleNum"
 # number of pixels
