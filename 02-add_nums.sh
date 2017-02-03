f7506ae548b64fc0073edd0a2324c832a78c3101
Dave89rr <daleriro@msn.com>
2017-02-03 13:20:41 -0500

    Assignment 3
    Added a few lines to add-nums. Now it finds area given pixel width and
    height

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -11,5 +11,17 @@ sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
+echo ""
+echo "Enter width?"
+read width
+echo "Enter height?"
+read height
+
+area=$(( width * height ))
+echo "The number of pixels is $area"
+echo "Press Enter to Close File..."
+read final
+
+
 # exercise: ask the user for the width and height and present total
 # number of pixels
