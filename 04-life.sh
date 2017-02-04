af6320a1d145989f029444aa043a43cf43cdd7c0
kingoruga <koruga@ggc.edu>
2017-02-04 18:04:27 -0500

    Edit 04-Life.sh

--- 04-life.sh
+++ 04-life.sh
@@ -1,16 +1,14 @@
-
-echo "What is the meaning of life?"
-read meaning
-
-
-if [ "$meaning" -eq 42 ]; then
-   echo "Yes!, That is the meaning of life!"
-else
-   echo "Awww... You don't know the meaning of life"
-fi
-
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
 # exercise: write a script that prints whether it is 
 # morning or not
+
+echo "Is it between the hours of 12am and 11.59am? "
+read answer
+
+if [ "$answer" == yes ]; then
+	echo "It is morning"
+else
+	echo "It is not morning"
+fi
