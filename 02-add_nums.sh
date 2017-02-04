949fa68f2eb207c646d2363986e0a2197f8d83a6
Blake Norman <blakenorman37@gmail.com>
2017-02-04 13:13:41 -0500

    Check in redo fix

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,9 +13,9 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
-echo "Enter your height"
+echo "Enter your height of the object"
 read fourth
-echo "Enter your width"
+echo "Enter your width of the object"
 read fifth
 
 total=$(( fifth*fourth ))
