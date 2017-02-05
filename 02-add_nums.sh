e97218ef1495a4454e28d965bb2d341a67561a2c
Bess Burnett <bburnett2@ggc.edu>
2017-02-05 12:13:23 -0500

    homework2.1

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-echo "Enter your first number"
-read first
-echo "Enter your second number"
-read second
-echo "Enter your third number"
-read third
+#echo "Enter your first number"
+#read first
+#echo "Enter your second number"
+#read second
+#echo "Enter your third number"
+#read third
 
-sum=$(( first+second+third ))
+#sum=$(( first+second+third ))
 
-echo "The sum is $sum"
+#echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "Enter width"
+read w
+echo "Enter height"
+read h
+
+totP=$(( h*w ))
+echo "The total number of pixels is "$totP
