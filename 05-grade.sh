1c98ae5c91ea1f96eb9f07a075f907ef7c2ed096
Default User <user@default.com>
2017-02-05 02:16:34 -0500

    Trying this again

--- 05-grade.sh
+++ 05-grade.sh
@@ -4,6 +4,7 @@
 echo "What did you get in the first ITEC 3860 test?"
 echo "Please give a numeric answer"
 read grade
+read temperature
 
 if [ $grade -ge 90 ]; then
    echo "You got an A. Nice."
@@ -15,6 +16,17 @@ else
    echo "Time to work on some extra credit assignments"
 fi
 
+
+echo "Place temp in numerics"
+if [ $temperature -ge 70 ]; then
+   echo "it's hot"
+elif [ $temperature -ge 60 ]; then
+   echo "it's okay"
+elif [ $temperature -ge 40 ]; then
+   echo "it's chilly"
+else
+   echo "it's cold"
+fi
 # exercise: write a script (that utilizes weather-util) 
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
