3c99df87e9e634f5f6569a269cc2d7f715d44403
apancha <apanchatcharam@ggc.edu>
2017-09-18 20:48:04 -0400

    made changes

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+	
+echo "Enter a width:"
+read width
+echo "Enter a Height:"
+read height
+
+pixels=$(( width*height ))
+
+echo "The total pixels are $pixels"
