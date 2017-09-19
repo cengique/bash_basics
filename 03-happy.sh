79a0d1c09acb0950a2403d69b0a1e8ef4408fd79
“Sagar” <31051996+sagarhansalia@users.noreply.github.com>
2017-09-19 11:41:38 -0400

    Updated the happy file

--- 03-happy.sh
+++ 03-happy.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-echo "You are happy?"
-read answer
-
-if [ "$answer" = "yes" ]; then
-   echo "Smile :)"
-else
-   echo "Still Smile :)"
-fi
+#echo "You are happy?"
+#read answer
+#
+#if [ "$answer" = "yes" ]; then
+#   echo "Smile :)"
+#else
+#   echo "Still Smile :)"
+#fi
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+echo "Is it the weekend?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Awesome!"
+else
+   echo "Aw man"
