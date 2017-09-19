050a210149d5b843f93c33eb84584b3c2022c7a4
cwindham19 <cwindham1@ggc.edu>
2017-09-18 22:08:54 -0400

    Branch Testing

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,11 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Enter the height"
+read tall
+echo "Enter the width"
+read wide
+
+pixels=$(( wide*tall ))
+
+echo "You have $pixels pixels in your square"
