f301a07ee0e35b5e76a08ed31518c006aa485528
aknobloch <aaronknobloch92@gmail.com>
2017-09-19 18:29:17 -0400

    Parallel universes

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,8 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+echo "Okay. Now give us a file name!"
+read filename
+
+touch "$PWD/$filename"
+echo "Done! Created a file called $filename in the directory $PWD"
