233d9c91764d2e927bb98894ec30cefe1e57b018
Chelsea D'Alessandro <cdalessandro@ggc.edu>
2017-09-19 23:21:25 -0400

    Changes to script 01

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -4,9 +4,10 @@
 echo "Hello! What is your name"
 read name
 echo "Welcome, $name"
+echo "Cheksea was here"
 
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
 
-# exercise: write a script that asks the user for a 
+# exercise: write a script that asks the user for a
 # filename and create an empty file named after it
