e7d5f84120a51b7a24d8e55940a15bd539b1622a
Janna Timmer <timmerjanna@gmail.com>
2017-09-19 22:29:41 -0400

    Calculate number of pixels

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,10 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Please enter width: "
+read width
+echo "Please enter height: "
+read height
+
+pixels = $(( width*height ))
+echo "Your number of pixels is $pixels!"
