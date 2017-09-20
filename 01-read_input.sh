ebe257ac5717c4c5345e26a225a386b88393e47a
Gabriel Mezquita <mezquitagabriel919@hotmail.com>
2017-09-19 22:05:10 -0400

    Part1 parallel iniverses #3: did exercise and pushed changes

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -8,5 +8,10 @@ echo "Welcome, $name"
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
 
-# exercise: write a script that asks the user for a 
+# exercise: write a script that asks the user for a
 # filename and create an empty file named after it
+
+echo "Please enter a filename: "
+read filename
+touch $filename.txt
+echo "A file named $filename.txt was created."
