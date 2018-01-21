974c6cdbd2b2acaf87a5cafc2a8f31696b2b86a9
Katrina Smith <ksmith10@ggc.edu>
2018-01-21 18:49:34 -0500

    Created script to prompt and create a file name from user

--- 01-read_input_copy.sh
+++ 01-read_input_copy.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-# read the name of the user and print hello
+# read the name of the a file
 
-echo "Hello! What is your name"
-read name
-echo "Welcome, $name"
+#echo "Hello! What is your name"
+#read name
+#echo "Welcome, $name"
 
 # single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
+#echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
+
+echo "Enter a file name to create with or without extention type"
+read fileName
+
+#create the file name
+touch  $fileName
+
+#display the file name
+echo "You just created: $fileName"
+
+
+
