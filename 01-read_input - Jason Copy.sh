54dcc59a1a74b0f826951f930050850064800f53
Charibasa <jyu1@ggc.edu>
2018-01-22 13:12:02 -0500

    Updated copy

--- 01-read_input - Jason Copy.sh
+++ 01-read_input - Jason Copy.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 # read the name of the user and print hello
 
-echo "Hello! What is your name?"
-read name
-echo "Welcome, Jason"
-
-# single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
-
-# exercise: write a script that asks the user for a 
-# filename and create an empty file named after it
-echo "Enter a name for a text, not including the .txt extension"
-read filename
-touch $filename".txt"
+echo "Hello! My name is Jason."
