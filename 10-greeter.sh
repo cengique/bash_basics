08f422f11f96ab6dc0c5096bc60d791fa2695a3d
Dave Chesser <dchesser@ggc.edu>
2018-02-27 11:54:25 -0500

    refactor: greeter is much nicer

--- 10-greeter.sh
+++ 10-greeter.sh
@@ -1,26 +1,11 @@
 #/bin/sh
 
-# function in bash
+name=$(test -z "$1" && echo `whoami` || echo "$1")
+# minor gripe: echo/printf just to compute variables
 
-function weekend_greet 
-{
-   echo "Enjoy the weekend! ${1}!"
-}
-
-function weekday_greet
-{
-   echo "Hope you are working hard, ${1}!"
-}
-
-
-echo "Please enter your name"
-read name
-
-day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
-
-if [ $day = "Sat" ] || [ $day = "Sun" ]; then
-   weekend_greet $name
+if [ `date +%Ou` -gt 5 ]
+then
+    printf "%s is finally in the weekend!\n" "$name"
 else
-   weekday_greet $name
+    printf "Working hard, %s?\n" "$name"
 fi
-
