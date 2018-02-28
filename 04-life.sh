787664d118a30d947ee729a1dd801b3f9ef3da66
swilliams21 <swilliams21@ggc.edu>
2018-02-28 11:45:44 -0500

    assignment 3 - 1

--- 04-life.sh
+++ 04-life.sh
@@ -9,6 +9,15 @@ else
    echo "Awww... You don't know the meaning of life"
 fi
 
+echo "what time is it?"
+read time
+
+if [ "$time" -lt 11 ]; then
+   echo "It is morning"
+else
+   echo "It is not morning"
+fi
+
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
