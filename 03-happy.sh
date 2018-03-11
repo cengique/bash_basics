148a8303c2167643102368525511c940daa79e93
Ariella Hernandez <ariellahdz13@gmail.com>
2018-03-11 14:28:47 -0400

    changed the output message

--- 03-happy.sh
+++ 03-happy.sh
@@ -8,9 +8,15 @@ if [ "$answer" = "yes" ]; then
 else
    echo "Still Smile :)"
 fi
-
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+ echo "Is today the weekend?"
+ read answer
+	if [ "$answer" = "yes" ]; then
+		echo "Enjoy your two days of freedom!"
+	else
+		echo "It's okay you're almost there!"
+fi 
