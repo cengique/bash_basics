af423d3599f3fd9ff134b222cfa3c86e4ba955ab
GianlucaParilli <gluka47@hotmail.com>
2018-03-11 21:20:21 -0400

    Lucas sum .sh file

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -9,7 +9,12 @@ read third
 
 sum=$(( first+second+third ))
 
-echo "The sum is $sum"
+if [ "$sum" = "47" ]; then
+  echo "Luca's sum is $sum"
+else
+   echo "Not luca's sum"
+fi
+
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
