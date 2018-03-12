d6f7f713a64f89d47ed029a0cc2352f7a452e61d
GianlucaParilli <gluka47@hotmail.com>
2018-03-11 21:32:24 -0400

    same file no if/else

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -9,12 +9,8 @@ read third
 
 sum=$(( first+second+third ))
 
-if [ "$sum" = "47" ]; then
-  echo "Luca's sum is $sum"
-else
-   echo "Not luca's sum"
-fi
-
+echo "Luca's sum is $sum"
+#no if/else
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
