bf2dae77f5f379d1e68e36044009cbd003e27711
Lusticar <sasam1996@gmail.com>
2018-03-13 10:33:14 -0400

    some more life changes

--- 04-life.sh
+++ 04-life.sh
@@ -9,8 +9,21 @@ else
    echo "Awww... You don't know the meaning of life"
 fi
 
+
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
 # exercise: write a script that prints whether it is 
 # morning or not
+h=`date +%H`
+
+date
+
+if [ $h -lt 12 ]; then
+  echo It is a Good morning
+elif [ $h -lt 18 ]; then
+  echo It is a Good afternoon
+else
+  echo It is a Good evening
+fi
+
