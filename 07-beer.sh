07276019aa11c2e3d7bdab035a85b11d0ef7db32
darthLu <lindsey.wade@me.com>
2018-10-15 19:45:04 -0400

    added line break to be different from the assignment branch

--- 07-beer.sh
+++ 07-beer.sh
@@ -22,3 +22,31 @@ done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+
+echo "--------------------------------------------------------------------"
+echo "Let's sing about Monkey's on the bed!"
+echo "How many monkey's are jumping on the bed?"
+read count
+
+while [ $count -ge 0 ]; do
+        if [ $count -ge 2 ]; then
+                echo "$count little monkey's jumping on the bed"
+                echo "one fell off and bumped his head!"
+                echo "momma called the doctor and the doctor said"
+                echo "NO MORE MONKEY'S JUMPING ON THE BED"
+        elif [ $count -eq 1 ]; then
+                echo "$count little monkey's jumping on the bed"
+                echo "one fell off and bumped his head!"
+                echo "momma called the doctor and the doctor said"
+                echo "NO MORE MONKEY'S JUMPING ON THE BED"
+        else
+                echo "One little monkey jumping on the bed,"
+                echo "She fell down and bumped her head,"
+                echo "Mama called the doctor and the doctor said,"
+                echo "Put those monkeys right to bed!"
+        fi
+
+
+        ((count = count - 1))
+
+done
