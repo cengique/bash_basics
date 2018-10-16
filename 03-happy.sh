7e2f6580e1ea4860226ad2fafd58a4714f3fced7
Yiet Mai <ymai@ggc.edu>
2018-10-16 18:18:25 -0400

    adding hungry function

--- 03-happy.sh
+++ 03-happy.sh
@@ -8,6 +8,15 @@ if [ "$answer" = "yes" ]; then
 else
    echo "Still Smile :)"
 fi
+echo "You are hungry?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "eat :)"
+else
+   echo "still eat some more :)"   
+
+fi
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
