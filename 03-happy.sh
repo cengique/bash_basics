e2d8adfb9bdeef746d21c20bb9bb3b9d2924cd39
amercadorodrigue <amercadorodrigue@ggc.edu>
2018-10-21 19:32:14 -0400

    Changed the 03-happy.sh

--- 03-happy.sh
+++ 03-happy.sh
@@ -8,9 +8,29 @@ if [ "$answer" = "yes" ]; then
 else
    echo "Still Smile :)"
 fi
+   
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+echo "What day of the week is it?"
+read day
+
+if [ "$day" = "Saturday" ]
+then
+   echo "It's the weekend! :D"
+elif [ "$day" = "saturday" ] 
+then
+   echo "It's the weekend! :D"
+elif [ "$day" = "Sunday" ]
+then
+   echo "It's the weekend! :D"
+elif [ "$day" = "sunday" ] 
+then
+   echo "It's the weekend! :D"
+else
+   echo "We'll make it to the weekend, don't worry ;)"
+fi
+   
