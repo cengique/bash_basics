fd2886280236ed746009c4f7f09769680e71cc06
pcardenasITEC-SOFTDEV <pcardenas0011@gmail.com>
2018-10-21 23:46:48 -0400

    Edited File (Paul)

--- 03-happy.sh
+++ 03-happy.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-echo "You are happy?"
-read answer
-
-if [ "$answer" = "yes" ]; then
-   echo "Smile :)"
-else
-   echo "Still Smile :)"
-fi
+ if [[ $(date +%u) -gt 5 ]] ; then
+ echo weekend
+ fi
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
