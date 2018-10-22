2eb36e135537be8d5a7a892b5fda8e9ede733c7f
Bradford Mashlum <bmashlum@gmail.com>
2018-10-22 16:38:05 -0400

    Assignment 6 parallel

--- 03-happy.sh
+++ 03-happy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+now=`date '+%A'`
+echo "Current date: $now"
+if [[ $now = "Saturday" || $now = "Sunday" ]]; then
+	echo "It's the weekend, CELEBRATE!"
+else
+	echo "It's not the weekend, BOO! :["
+fi
+
 echo "You are happy?"
 read answer
 
