033c1f9ebafae5fa7b81ee751cc948aba7e73a59
Bradford Mashlum <bmashlum@gmail.com>
2018-10-22 15:30:04 -0400

    Assignment 6

--- 03-happy.sh
+++ 03-happy.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-echo "You are happy?"
+now=`date '+%A'`
+echo "Current date: $now"
+if [[ $now = "Saturday" || $now = "Sunday" ]]; then
+	echo "It's the weekend!"
+else
+	echo "It's not the weekend :("
+fi
+
+echo "Are you happy?"
 read answer
 
 if [ "$answer" = "yes" ]; then
@@ -8,6 +16,9 @@ if [ "$answer" = "yes" ]; then
 else
    echo "Still Smile :)"
 fi
+
+
+
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
