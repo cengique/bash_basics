104893675be43db43ba1f97e1ed52798c74baecf
amercadorodrigue <amercadorodrigue@ggc.edu>
2018-10-21 20:20:24 -0400

    Changed 03-happy.sh on parallel branch

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,22 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+echo "What day of the week is it?"
+read day
+
+if [ "$day" = "Saturday" ]
+then
+   echo "It's the weekend! :D"
+elif [ "$day" = "saturday" ]
+then
+   echo "It's the weekend! :D"
+elif [ "$day" = "Sunday" ]
+then
+   echo "It's the weekend! :D"
+elif [ "$day" = "sunday" ]
+then
+   echo "It's the weekend! :D"
+else
+   echo "We'll make it to the weekend, don't worry ;)"
+fi
