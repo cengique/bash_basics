8ea26e5f228257864da7e4e5844fdbba3c12eaab
lwalsh3 <lwalsh2@ggc.edu>
2019-01-21 12:40:28 -0500

    New file, read_input_lwalsh2.sh runs and echos out my name.

---
+++ read_input_lwalsh2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! Your name is Lukas?"
+echo "Welcome, Lukas"
+
+# single quotes prevent the expansion of the variable
+#echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
