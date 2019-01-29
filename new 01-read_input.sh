5ed595b78a6f6a15614742b6fe4337452f129f55
Jared Easton <jeaston@ggc.edu>
2019-01-29 16:35:29 -0500

    added new 01-read_inut.sh

---
+++ new 01-read_input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! Jared Easton"
+
+# single quotes prevent the expansion of the variable
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
+echo enter a filename
+read theFileName
+touch $theFileName
+echo $theFileName has been created
