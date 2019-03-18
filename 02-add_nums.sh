dd20a2e29ed8052bab034241a2ed52d4440a116f
dsales Laptop <dsales@ggc.edu>
2019-03-18 11:44:32 -0400

    Modified file per git bash assignment

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo ""
+echo "Enter the width"
+read width
+echo "Enter the height"
+read height
+
+dimensions=$((width*height))
+
+echo "The number of pixels $dimensions"
