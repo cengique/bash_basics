d32859ad06b9fc7de50d17e2e55031e2c5174a24
dsales Laptop <dsales@ggc.edu>
2019-03-18 12:13:05 -0400

    new version from new branch of assignment

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo ""
+echo "Enter the X dimension first"
+read x
+echo "Enter the Y dimension now"
+read y
+
+dim=$((x*y))
+
+echo "The total number of pixels $dim"
