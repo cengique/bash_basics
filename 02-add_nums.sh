78782884ef9ca3d093aa0ce7c94039e02512b53b
Matthew Camp <mcamp1@ggc.edu>
2019-03-18 13:11:53 -0400

    changed add_nums

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -14,8 +14,12 @@ echo "The sum is $sum"
 # exercise: ask the user for the width and height and present total
 # number of pixels
 
-echo "width:"
-read width
-echo "height:"
-read height
-echo "Area: $((width * height))"
+echo "Now we are going to calculate the area of a screen in pixels"
+echo "Enter the width of the screen"
+read widthOfScreen
+echo "Enter the height of the screen"
+read heightOfScreen
+
+screenArea = $((widthOfScreen * heightOfScreen))
+
+echo "The area of your screen in pixels is $screenArea"
