9d35ace6785c053a1b43e6be65048f49c17af877
ajoseph11 <ajoseph@ggc.edu>
2019-03-19 16:58:20 -0400

    ,,

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -8,5 +8,7 @@ read heights
 pixel=$((widths*heights))
 
 echo "The tolal number of pixels $pixel"
+echo "Thank you I hope your picture turns out beautiful !"
+
 # exercise: ask the user for the width and height and present total
 # number of pixels
