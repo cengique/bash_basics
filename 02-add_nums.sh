9b86f7b28dfd7a4f52b68c25c67981e29d798209
Matthew Camp <mcamp1@ggc.edu>
2019-03-19 19:41:25 -0400

    Some more changes

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -2,12 +2,12 @@
 
 # This is a comment in the program because I need to make more changes
 
-echo "Enter a width"
+echo "Enter a width in pixels"
 read width
-echo "Enter a height"
+echo "Enter a height in pixels"
 read height
 
 area=$((width * height))
 
-echo "The area in pixels is $area"
+echo "It is $area pixels in area"
 echo "The program is finished"
