ed0cecde706f97e7dbce8575bb38977317b25b62
Jacob Smallwood <jacob.smallwood91@gmail.com>
2019-03-19 21:32:22 -0400

    Add screen area calculation to 02-add_nums.sh

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,11 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "Enter screen width in pixels"
+read width
+echo "Enter screen height in pixels"
+read height
+
+total_pixels=$(( width*height ))
+echo "The total number of pixels is $total_pixels"
