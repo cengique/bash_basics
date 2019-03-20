fa0638d12221956644fd55ec152d7c1338c7a212
ajoseph11 <ajoseph@ggc.edu>
2019-03-19 20:56:00 -0400

    Editing file

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-echo "Welcome, Enter a width!"
-read widths
-echo "Enter a height!"
-read heights
 
+echo "Enter your first number"
+read first
+echo "Enter your second number"
+read second
+echo "Enter your third number"
+read third
 
-pixel=$((widths*heights))
+sum=$(( first+second+third ))
 
-echo "The tolal number of pixels $pixel"
-echo "Thank you I hope your picture turns out beautiful !"
+echo "The sum is $sum"
 
+echo " Hello! Enter the width!"
+read width
+echo "Enter the height!"
+read height
+
+pixels=$((width*height))
+
+echo " The total number of pixels is $pixels "
 # exercise: ask the user for the width and height and present total
 # number of pixels
