9c620e9cafaf9846a97bdc067d6e5c583ebb461d
dmitchner <dmitchner@ggc.edu>
2019-05-01 21:12:01 -0400

    changed name

--- 09-name.sh
+++ 09-name.sh
@@ -2,10 +2,10 @@
 
 # look up ip addresses of various search engines
 
-servers="yahoo.com google.com dogpile.com wolframalpha.com"
+servers="yahoo.com  google.com"
 
 for server in $servers; do
-    nslookup $server
+    ping $server
     echo "----------------------------"
 done
 
