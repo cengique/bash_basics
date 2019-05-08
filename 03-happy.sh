06d47a3c71a46a8c71ecd61e128e6e92f9912a17
Austin <alomax@ggc.edu>
2019-05-07 23:42:51 -0400

    Updated

--- 03-happy.sh
+++ 03-happy.sh
@@ -8,12 +8,13 @@ if [ "$answer" = "yes" ]; then
 else
    echo "Still Smile :)"
 fi
-
+done
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+<<<<<<< HEAD
 echo "Is today the weekend?"
 read answer
 
@@ -22,3 +23,20 @@ if [ "$answer" = "yes" ]; then
   else
   echo "booooooo!!! :("
 fi
+=======
+
+echo "Is today the weekend?"
+
+read answer
+
+if [ "$answer" = "yes" ]; then
+
+   echo "Yay!! :)"
+
+else if [ "$answer" = "no" ]; then
+
+   echo "Boooo :("
+
+fi
+done
+>>>>>>> eb13ca58111c5f4920832dfd48ace9c1be022fdb
