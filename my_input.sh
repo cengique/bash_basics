c03b1b56499a81394521615c81316c0e64ee9c76
Austin <alomax@ggc.edu>
2019-05-07 22:16:39 -0400

    this is assignment 1

---
+++ my_input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read Austin
+echo "Welcome, Austin"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in Austin'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
