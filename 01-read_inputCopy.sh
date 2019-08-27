7158ce86d93aa5302ec6a671ba4b36fc0b14227c
sebastianp95 <sebastian_perez95@hotmail.com>
2019-08-27 11:31:48 -0400

    Print name Bash Example, Sebastian perez

---
+++ 01-read_inputCopy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Sebastian Perez, "
+echo "Hello! What is your name"
+read name
+echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
+echo "Hey $name, What is your filename?"
+read filename
+echo "Creating $filename ..."
+touch $filename
+echo "File created: $filename"
+ls
