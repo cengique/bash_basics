f0daa842b2b5949910397187c70fb1c48dbc61ac
weaponized <lle4@ggc.edu>
2019-08-27 15:32:14 -0400

    first commit and edit

---
+++ 01-create_file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name
+echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+echo  "Enter a filename"
+read filename
+echo "Your filename is $filename"
+touch $filename.txt
