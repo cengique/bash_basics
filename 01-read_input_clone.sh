ee04eeb3be9e1ddde2d2424016870229dbf4cba1
Jose <jcantillo@ggc.edu>
2019-08-30 11:05:32 -0400

    git/bash Jose's assignment

---
+++ 01-read_input_clone.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name
+echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
+chmod +x (/Users/jcantillo/Documents/01-read_input_clone.sh)
