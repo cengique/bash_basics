2b6f4e1499287821c8b09e156cdb096a65451f19
nsardar <nsardar@ggc.edu>
2019-09-09 23:48:28 -0400

    Try 4 of the assignment in new U

--- 10-greeter.sh
+++ 10-greeter.sh
@@ -12,9 +12,23 @@ function weekday_greet
    echo "Hope you are working hard, ${1}!"
 }
 
-
 echo "Please enter your name"
 read name
+
+echo "Enter [1-4] what year of college you're currently at" 
+read year
+
+if [ $year = "1" ]; then
+	echo "You're a freshman!" 
+elif [ $year = "2" ]; then 
+	echo "You're a sophomore!" 
+elif [ $year = "3" ]; then 
+	echo "You're a junior!"
+elif [ $year = "4" ]; then 
+	echo "You're a senior!" 
+else
+	echo "You didn't enter something right." 
+fi
 
 day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
 
