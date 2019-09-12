983b21e00f5df164d754138e7acdc310168a341e
[Jacob Wise] <[jwise@ggc.edu]>
2019-09-12 17:50:07 -0400

    added screen pixel calculation

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -8,11 +8,11 @@ echo "Enter your third number"
 read third
 sum=$(( first+second+third ))
 
-echo "Enter screen width"
-read width
-echo "Enter screen height"
-read height
-pixels=$((width*height))
+echo "Enter screen x"
+read x
+echo "Enter screen y"
+read y
+pixels=$((x*y))
 
 echo "The sum is $sum"
 echo "The number of pixels are $pixels"
