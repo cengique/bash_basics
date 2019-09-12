aea33b3c95661213592d21bf9df3b2af77e28873
[Jacob Wise] <[jwise@ggc.edu]>
2019-09-12 17:42:59 -0400

    added a function to calculate pixels in screen

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -6,10 +6,16 @@ echo "Enter your second number"
 read second
 echo "Enter your third number"
 read third
-
 sum=$(( first+second+third ))
 
+echo "Enter screen width"
+read width
+echo "Enter screen height"
+read height
+pixels=$((width*height))
+
 echo "The sum is $sum"
+echo "The number of pixels are $pixels"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
