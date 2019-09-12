0c5080d6f89f0f9d91629b3722f434535958ee21
Mark Levengood <mlevengood@ggc.edu>
2019-09-11 22:23:58 -0400

    Added new commands to script.

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,9 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "Enter width"
+read width
+echo "Enter height"
+read height
+echo "Present total number of pixels: $((width * height))"
