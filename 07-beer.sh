e2e1ae5c48326ec880a1dd74ee813fbf4e667edd
Josue Pastore-Manzano <jpastoremanzano>
2019-09-12 21:08:15 -0400

    I did it 0 bowls of chicken on the wall

--- 07-beer.sh
+++ 07-beer.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-echo "Let's sing a beer song"
-echo "How many bottles?"
+echo "Let's sing a chicken song"
+echo "How many bowls?"
 read count
 
 while [ $count -ge 0 ]; do
   if [ $count -ge 2 ]; then
-      echo "$count bottles of beer on the wall, $count bottles of beer"
+      echo "$count bowls of chicken on the wall, $count bowls of chicken"
       echo "Take one down pass it around"
   elif [ $count -eq 1 ]; then
-      echo "$count bottle of beer on the wall, $count bottles of beer"
+      echo "$count bowls of chicken on the wall, $count bowls of chicken"
       echo "Take one down pass it around"
   else
-      echo "no more bottles of beer on the wall"
+      echo "no more bowls of chicken on the wall"
   fi
-  
+
   # the following statement is equivalent to: let "count=count-1"
   ((count = count - 1))
 
 done
 
-# exercise: implement another counting song (such as 12 days of Christmas) 
+# exercise: implement another counting song (such as 12 days of Christmas)
 # using loops and if statements.
