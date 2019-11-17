b9502143cc6ca111962cc34f92afbda3de7c5b5b
Raghu Khanal <Khanalraghu03@gmail.com>
2019-11-17 13:51:58 -0500

    added a program that multiplies three nums

---
+++ 02-multiply_nums.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "This program will multiply three numbers"
+echo "Enter your first number"
+read first
+echo "Enter your second number"
+read second
+echo "Enter your third number"
+read third
+
+multiply=$((first)(second)(third)))
+
+echo "The sum is $multiply"
+
+# exercise: ask the user for the width and height and present total
+# number of pixels
