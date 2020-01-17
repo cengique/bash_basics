07a8b0591fb7a581a6af20e197d271eeb4b64cbb
zidrees <zidrees@ggc.edu>
2020-01-16 20:25:56 -0500

    First commit

---
+++ 01-read_input_copy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+name= "Zachary Idrees"
+#read name
+echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+echo "give a name to name your file"
+read filename
+touch $filename
