4db530e6d323d7f31c838984754d25f672ed11c6
fahimchoud <fchoudhury@ggc.edu>
2020-01-18 14:38:11 -0500

    adding a change of name Luka replace just a blank of name

---
+++ remyes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name
+echo "Welcome, Luka"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in Luka'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
