dfd799db1edf511aca40287a287564ac81b3c91e
Ryan Cosentino <ryan@6397.local>
2020-01-21 00:45:20 -0500

    Created a new script which makes files.

---
+++ 01-read_input-ryan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "I'm Ryan Cosentino. What is the name of a file you'd like to create?"
+read filename
+touch $filename
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
