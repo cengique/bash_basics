7a9477bb55d73708c9c0a768fee433807b118175
madams27 <madams27@ggc.edu>
2020-01-27 18:31:47 -0500

    parallel snapshots

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,20 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+function weekend 
+{
+   echo "The weekend is here!! :)"
+}
+
+function no_weekend
+{
+	echo "Sorry, no weekend for u. :0"
+}
+
+day=$(date | cut -d' ' -f1)
+if [ $day = "Sat" ] || [ $day = "Sun" ]; then
+   weekend
+else
+   no_weekend
+fi
