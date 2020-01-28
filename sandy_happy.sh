e286b26dafd32b9cc97e1b4fbbd753304ea501e9
Sandy Ali <sandyali@Sandys-MacBook-Pro-2.local>
2020-01-27 19:32:26 -0500

    made a copy of happy into sandy's version

---
+++ sandy_happy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "You are happy?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Smile :)"
+else
+   echo "Sandy will Smile for you :)"
+fi
+
+# here are the other string comparison operators
+# != , -n (not an empty string) , -z (an empty string)
+
+# exercise: write a script that prints whether today is
+# the weekend or not
