8dcf0b0489069e58d981a183afc1f2cf65e98688
Khang Ho <themarinect@gmail.com>
2020-01-28 21:48:21 -0500

    Make changes in master

--- 09-name.sh
+++ 09-name.sh
@@ -5,7 +5,11 @@
 servers="amazon.com ebay.com cnn.com altervista.org"
 
 for server in $servers; do
+<<<<<<< Updated upstream
     traceroute $server
+=======
+    nslookup $server
+>>>>>>> Stashed changes
     echo "----------------------------"
 done
 
