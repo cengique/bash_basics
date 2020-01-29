b354c5f4e69083d212d23af81cca15a80907e661
Khang Ho <themarinect@gmail.com>
2020-01-28 21:10:41 -0500

    Make changes in 02_add_nums.sh

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -7,7 +7,7 @@ read height
 
 pixels=$(( width*height ))
 
-echo "The total number of pixels is $pixels"
+echo "The number of pixels is $pixels"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
