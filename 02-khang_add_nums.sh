5f525729381d94756e80fd32bc7c2fbd7c0a4bf3
themarinect <themarinect@gmail.com>
2020-01-28 19:07:13 -0500

    Create 02-khang_add_nums.sh

---
+++ 02-khang_add_nums.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Enter the width"
+read width
+echo "Enter the height"
+read height
+
+pixels=$(( width*height ))
+
+echo "The number of pixels is $pixels"
+
+# exercise: ask the user for the width and height and present total
+# number of pixels
