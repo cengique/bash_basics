6050152e58803a733f5a520c89db201c8ac4a9f6
Matheus <msilva7@ggc.edu>
2020-01-29 19:10:22 -0500

    02 changed

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-echo "Enter your first number"
-read first
-echo "Enter your second number"
-read second
-echo "Enter your third number"
-read third
+# echo "Enter your first number"
+# read first
+# echo "Enter your second number"
+# read second
+# echo "Enter your third number"
+# read third
 
-sum=$(( first+second+third ))
+# sum=$(( first+second+third ))
 
-echo "The sum is $sum"
+# echo "The sum is $sum"
+
+echo "What is the width of you screen?"
+read width
+echo "What is the height of the screen?"
+read height
+
+screen=$(((height+width)/2))
+
+echo "Your screen size is $screen inches"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
