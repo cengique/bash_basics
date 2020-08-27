c1327bb225f49c8cda66aeac89f7744b526ca885
Dennis Dalton <ddalton3@ggc.edu>
2020-08-27 15:58:23 -0400

    Image plus file changes

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -11,5 +11,13 @@ sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
+echo "Enter the width on your screen"
+read width
+echo "Enter the height of your screen"
+read height
+
+pixels=$(( width*height ))
+
+echo "The total number of pixels is: $pixels"
 # exercise: ask the user for the width and height and present total
 # number of pixels
