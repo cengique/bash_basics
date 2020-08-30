1d2c71a075849787caa421931ae41ee576854008
Fawhn Hanlon <whanlon@ggc.edu>
2020-08-29 22:57:49 -0400

    adds script to print whether it is weekend or not

--- 03-happy.sh
+++ 03-happy.sh
@@ -3,7 +3,7 @@
 echo "You are happy?"
 read answer
 
-if [ "$answer" = "yes" ]; then
+  if [ "$answer" = "yes" ]; then
    echo "Smile :)"
 else
    echo "Still Smile :)"
@@ -14,3 +14,14 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+echo 
+echo "What day is it?"
+read answer
+
+if [ "$answer" = "sunday" ]; then
+  echo "it is the weekend! :)"
+elif [ "$answer" = "saturday" ]; then
+  echo "it is the weekend! :)"
+else
+  echo "it is not the weekend. :("
+fi
