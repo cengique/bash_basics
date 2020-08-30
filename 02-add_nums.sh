0abe2a4f640c9e8e4052978e756ff01775e86836
jmayorga1 <jmayorga1@ggc.edu>
2020-08-30 18:21:50 -0400

    added fourth number that divides the first three- Josue Mayorga

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -6,10 +6,12 @@ echo "Enter your second number"
 read second
 echo "Enter your third number"
 read third
-
-sum=$(( first+second+third ))
-
-echo "The sum is $sum"
+echo "Enter your last number"
+read fourth
+total=$(( first+second+third ))
+div=$((fourth))
+sum=$(($total/$div))
+echo "The sum of the first three numbers divided by the fourth number is  $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
