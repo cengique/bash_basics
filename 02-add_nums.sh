8d93cd9157cf8e3caca29ecd9aad831a2bf1b067
Francisco <fpomayaramos@ggc.edu>
2020-08-31 02:28:23 -0400

    Excercise 2 modified

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -7,9 +7,19 @@ read second
 echo "Enter your third number"
 read third
 
-sum=$(( first+second+third ))
+sum=$(( first+second+third+fourth ))
 
 echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "Now, enter the width of a figure"
+read width
+echo "Now, enter the height of a figure"
+read height
+
+# Not sure if by number of pixels you mean area, but here it goes
+total=$(( width*height ))
+
+echo "Total number of pixels is $total"
