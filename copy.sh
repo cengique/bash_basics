86f3561c2ccca3618d809558f7b928c8022a551f
aissak <aissak@ggc.edu>
2020-10-03 03:13:37 -0400

    New copy file created

---
+++ copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name 
+echo "Welcome, Asho"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in Asho'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
